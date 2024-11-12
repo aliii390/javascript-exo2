eb95eb978e5f5d73214b4f61a30f81c772eac51c
aliii390 <alli.said.errahmani@gmail.com>
2024-11-12 16:58:11 +0100

    ADD: debut de l'exercice

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+let lastName = document.getElementById('#lastname');
+let firstName = document.getElementById('#firstname');
+let city = document.getElementById('#city');
+
+
+let submit = document.getElementById('#submit')
+
+function chat(lastName, firstName, city) {
+    
+
+    
+}
+
+ 
+

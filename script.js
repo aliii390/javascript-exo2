25412849b289a3367b9b16b90804a34e50e766d5
aliii390 <alli.said.errahmani@gmail.com>
2024-11-13 09:52:32 +0100

    ADD: fin de l'exo 4

--- script.js
+++ script.js
@@ -3,18 +3,31 @@
 
 
 
-let lastName = document.getElementById('#lastname');
-let firstName = document.getElementById('#firstname');
+ let lastName = document.getElementById('#lastname');
+ let firstName = document.getElementById('#firstname');
 let city = document.getElementById('#city');
 
 
-let submit = document.getElementById('#submit')
-
-function chat(lastName, firstName, city) {
-    
-
-    
-}
 
  
+
+ document.getElementById("submit").addEventListener("click", function(Bonjour){
+     let nom = "";
+   while(!nom){
+       nom = prompt("Quel est votre nom");
+    }
+
+    const confirmation = confirm(`etes vous sur que  ${nom} est votre nom`);
+
+    if(confirmation){
+       alert(`Bonjour  ${nom}`) ;
+    }
+ })
+
+
+
+
+
+
+
 

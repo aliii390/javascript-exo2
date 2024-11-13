a97f98c9de03672437754722d9f886dea57597e7
aliii390 <alli.said.errahmani@gmail.com>
2024-11-13 16:18:33 +0100

    ADD: fin de l'exercice 3 algo-confirmer compteur

---
+++ algo-confirmer/exo3/script.js
@@ -0,0 +1,20 @@
+
+
+
+// Créer une boucle while qui simule un compte à rebours de 10 à 0, puis affiche "Décollage !" à la fin.
+
+
+
+
+    let debut = 10;
+    
+    while(debut > 0 ){
+        console.log(debut)
+         debut -- ;
+
+    }
+
+    console.log("décollage !")
+
+ 
+

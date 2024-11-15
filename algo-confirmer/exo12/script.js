509e24c912a72ae2565593fe26e10949609e2701
aliii390 <alli.said.errahmani@gmail.com>
2024-11-15 15:35:22 +0100

    ADD: fin de l'exo12 il reste des modif a faire

---
+++ algo-confirmer/exo12/script.js
@@ -0,0 +1,22 @@
+
+let tableau = [];
+
+let number;
+
+for(let i = 0;  i <= 20; i += 1){
+    tableau += Number(prompt("Veuillez saisir un nombre : "));
+     console.log(tableau);
+
+} 
+
+
+
+
+
+
+
+
+
+
+
+

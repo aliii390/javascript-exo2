c152e63a9f64eb280647d6d63521e0a136563d6c
aliii390 <alli.said.errahmani@gmail.com>
2024-11-15 10:00:20 +0100

    ADD: fin de l'exercice 10

--- algo-confirmer/exo10/script.js
+++ algo-confirmer/exo10/script.js
@@ -1,2 +1,13 @@
-const user = parseInt(prompt("Entrez un nombre : "));
-let resultat = "";
+let number;
+
+while(Number.isInteger(number) === false ){
+    number = Number(prompt("Veuillez saisir un nombre : "));
+}
+
+let resultat = `La table de ${number}  : \n`;
+
+for (let i = 1; i <= 10; i += 1) {
+  resultat += number + " X "+ i + " = " + number * i + "\n";
+}
+
+alert(resultat);
